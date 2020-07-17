7b365915533737907bb0b665be8eb17e30638667
Lauren Stone <lstone321@gmail.com>
2020-07-17 04:00:55 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -13,3 +13,13 @@ else
   return "This block should not run!"
 end 
 end 
+
+
+def my_each(arr) 
+  counter = 0 
+  while counter < arr.length 
+  yield (arr[counter])
+  counter += 1
+  end
+  arr
+end
